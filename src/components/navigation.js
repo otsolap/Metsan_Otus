5f9e3083b07a7c53c1bbc266bf61278321fd3e0a
OtsoLap <otsolappalainen@gmail.com>
2021-05-02 09:51:42 +0300

    navigation wip static query.

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,38 +1,85 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql, StaticQuery, Link } from "gatsby"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 import NavDropdown from "react-bootstrap/NavDropdown"
 
-const MenuLinks = ({ navQuery }) => {
-  navQuery.map((link) => (
-    <Nav.Link as="li" key={link.title}>
-      <Link
-        to={link.link}
-      >
-        {link.title}
-      </Link>
-      {link.subMenu && link.subMenu.length > 0 ? (
-        <NavDropdown class="sub-items responsive-navbar-nav">
-          {link.subMenu.map((sublink) => (
-            <NavDropdown.Item a href={sublink.link}>
-              {sublink.title}
-            </NavDropdown.Item>
-          ))}
-        </NavDropdown>
-      ) : null}
-    </Nav.Link>))
+function menuLinks() {
+  return (
+    <StaticQuery
+      query={graphql`
+      query menuItems {
+        site {
+          siteMetadata {
+            MenuLinks {
+              link
+              title
+              subMenu {
+                link
+                title
+              }
+            }
+          }
+        }
+      }
+      `}
+      render={data => (
+        <ul>
+          {data.site.siteMetadata.Menulinks.map((link) => (
+            <Nav.Link as="li" key={link.title}>
+              <Link
+                to={link.link}
+              >
+                {link.title}
+              </Link>
+              {link.subMenu && link.subMenu.length > 0 ? (
+                <NavDropdown class="sub-items responsive-navbar-nav">
+                  {link.subMenu.map((sublink) => (
+                    <NavDropdown.Item a href={sublink.link}>
+                      {sublink.title}
+                    </NavDropdown.Item>
+                  ))}
+                </NavDropdown>
+              ) : null}
+            </Nav.Link>))
+          }</ul>
+      )}
+    />
+  )
 }
 
+
+
 class Navigation extends React.Component {
+
   render() {
+    /*   const MenuLinks = ({ navQuery }) => {
+        navQuery.map((link) => (
+          <Nav.Link as="li" key={link.title}>
+            <Link
+              to={link.link}
+            >
+              {link.title}
+            </Link>
+            {link.subMenu && link.subMenu.length > 0 ? (
+              <NavDropdown class="sub-items responsive-navbar-nav">
+                {link.subMenu.map((sublink) => (
+                  <NavDropdown.Item a href={sublink.link}>
+                    {sublink.title}
+                  </NavDropdown.Item>
+                ))}
+              </NavDropdown>
+            ) : null}
+          </Nav.Link>))
+      }
+   */
     return (
       <Navbar collapseOnSelect expand="md" className="site-navigation">
         <Navbar.Brand class="logo" href="/">Metsän Otus</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <ul>
-            {MenuLinks}
+            {menuLinks}
           </ul>
         </Navbar.Collapse>
       </Navbar >
